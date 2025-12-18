71022cd8334cd87a5c438ef7d7759c4b70c4434f
KaigorodovMM <kai-tram-pam-pam@mail.ru>
2025-12-18 23:24:14 +0500

    edit data_stats task

--- 03_week/tasks/data_stats/data_stats.cpp
+++ 03_week/tasks/data_stats/data_stats.cpp
@@ -8,7 +8,7 @@ struct DataStats {
     double sd = 0.0;
 };
 
-DataStats CalculateDataStats(std::vector<int> vect) {
+DataStats CalculateDataStats(std::vector<int>& vect) {
     int n = 0;
     
     DataStats rezult = {0, 0};
@@ -26,5 +26,24 @@ DataStats CalculateDataStats(std::vector<int> vect) {
     rezult.sd /= n;
     rezult.sd = sqrt(rezult.sd);
     return rezult;
+}
 
+DataStats CalculateDataStats(const std::vector<int>& vect) {
+    int n = 0;
+    
+    DataStats rezult = {0, 0};
+    if (vect.begin() == vect.end()){
+        return rezult;
+    }
+    for (auto it = vect.begin(); it != vect.end(); ++it) {
+        ++n;
+        rezult.avg += *it;
+    }
+    rezult.avg /= n;
+    for (auto it = vect.begin(); it != vect.end(); ++it) {
+        rezult.sd += pow((*it - rezult.avg), 2);
+    }
+    rezult.sd /= n;
+    rezult.sd = sqrt(rezult.sd);
+    return rezult;
 }
